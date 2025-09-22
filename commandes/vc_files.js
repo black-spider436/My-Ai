f7e54d7066c325ec29f98cb28802ff6253e59577
sir bravin <toxiclover416@gmail.com>
2025-09-22 19:00:59 +0300

    Create vc_files.js

---
+++ commandes/vc_files.js
@@ -0,0 +1,234 @@
+const { bravin } = require("../sir/bravin");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../sir/bravin");
+const { default: axios } = require('axios');
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+bravin({ nomCom: 'vcf',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📝', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_sorry just use 👉 vcf10 to get all contact_*`) 
+
+   
+
+
+  }
+);
+
+
+bravin({ nomCom: 'getallmembers',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '🤔', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_getting all members_*`) 
+
+   
+
+
+  }
+);
+
+
+
+bravin({ nomCom: 'channel',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '⚡️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`Support Here My Owner By Follow This Channel Please :https://whatsapp.com/channel/0029VbAjawl9MF8vQQa0ZT32`) 
+
+   
+
+
+  }
+);
+
+
+bravin({ nomCom: 'group1',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '🗣', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*Tap Here To Join ʟᴇᴏɴᴀʀᴅ ᴍᴅ Chatroom* https://whatsapp.com/channel/0029VbAjawl9MF8vQQa0ZT32`) 
+
+   
+
+
+  }
+);
+
+
+bravin({ nomCom: 'update',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '⚙️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_MY AI is running on its latest vision_*`) 
+
+   
+
+
+  }
+);
+
+
+bravin({ nomCom: 'vision',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📡', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_myself_*`) 
+
+   
+
+
+  }
+);
+
+
+  
+bravin({ nomCom: 'leonard',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '🥷', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*ᴛᴇʟʟ ᴍʏ ɢᴜʏ*`) 
+
+   
+
+
+  }
+)
+
+
+bravin({ nomCom: 'hack2',
+    desc: 'To check runtime',
+    Categorie: 'My Contact',
+    reaction: '⚠️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`Injecting Malware",
+    " █ 10%",
+    " █ █ 20%",
+    " █ █ █ 30%",
+    " █ █ █ █ 40%",
+    " █ █ █ █ █ 50%",
+    " █ █ █ █ █ █ 60%",
+    " █ █ █ █ █ █ █ 70%",
+    " █ █ █ █ █ █ █ █ 80%",
+    " █ █ █ █ █ █ █ █ █ 90%",
+    " █ █ █ █ █ █ █ █ █ █ 100%",
+    "System hijacking on process..\nConnecting to Server error to find 404",
+    "Device successfully connected...\nReceiving data...",
+    "Data hijacked from device 100% completed\nKilling all evidence, killing all malwares...",
+    "HACKING COMPLETED",
+    "SENDING LOG DOCUMENTS...",
+    "SUCCESSFULLY SENT DATA AND Connection disconnected",
+    "BACKLOGS CLEARED",
+    "POWERED BY LEONARD MD",
+    "By Leonard Tech`) 
+
+   
+
+
+  }
+)
+
+
+
+
+bravin({ nomCom: 'problem',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '❄', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+
+                 await repondre(`*_┏━━━━━━━━━━━━━━
+┃myself 🎉🎉🎉 
+| ɴɴᴇᴠᴇʀ 
+┃ᴅɪᴇ🔥
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❶ || Creator = 𖥘 myself 𖥘
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VbB4nox4Y9lqVl2X8n3m
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+Please Follow My Support Channel
+Wanna talk to me?👉 071726***** 👈
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+©*_2024-2099 ʟᴇᴏɴᴀʀᴅ ᴛᴇᴄʜ_*`) 
+
+   
+
+
+  }
+);
+
