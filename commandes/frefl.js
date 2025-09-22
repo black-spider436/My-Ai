9365d71e3469dc94a7f156a52ce8e9d4c6defde3
sir bravin <toxiclover416@gmail.com>
2025-09-22 17:20:14 +0300

    Create frefl.js

---
+++ commandes/frefl.js
@@ -0,0 +1,417 @@
+function _0x539d(_0x2871f4, _0x57dc69) {
+  const _0x15ea92 = _0x7635();
+  _0x539d = function (_0x9697d8, _0x2e8755) {
+    _0x9697d8 = _0x9697d8 - 171;
+    let _0x598f41 = _0x15ea92[_0x9697d8];
+    return _0x598f41;
+  };
+  return _0x539d(_0x2871f4, _0x57dc69);
+}
+(function (_0x5976e4, _0x24fb79) {
+  const _0x5c308b = _0x5976e4();
+  while (true) {
+    try {
+      const _0x5c9316 = -parseInt(_0x539d(193, 0x2ab)) / 1 * (-parseInt(_0x539d(385, 0x3ba)) / 2) + parseInt(_0x539d(593, 0x429)) / 3 * (parseInt(_0x539d(486, -0x78)) / 4) + parseInt(_0x539d(190, -0x275)) / 5 + -parseInt(_0x539d(187, 0x290)) / 6 * (-parseInt(_0x539d(344, -0x14e)) / 7) + -parseInt(_0x539d(308, 0x2d)) / 8 * (-parseInt(_0x539d(403, -0x17a)) / 9) + parseInt(_0x539d(398, 0x23c)) / 10 + -parseInt(_0x539d(597, 0x4d3)) / 11;
+      if (_0x5c9316 === _0x24fb79) {
+        break;
+      } else {
+        _0x5c308b.push(_0x5c308b.shift());
+      }
+    } catch (_0x5e41b4) {
+      _0x5c308b.push(_0x5c308b.shift());
+    }
+  }
+})(_0x7635, 357706);
+const _0x349748 = function () {
+  let _0x557f01 = true;
+  return function (_0x5aead5, _0x376646) {
+    const _0xae4e06 = _0x557f01 ? function () {
+      if (_0x376646) {
+        const _0x1bd819 = _0x376646.apply(_0x5aead5, arguments);
+        _0x376646 = null;
+        return _0x1bd819;
+      }
+    } : function () {};
+    _0x557f01 = false;
+    return _0xae4e06;
+  };
+}();
+const _0x3022af = _0x349748(this, function () {
+  return _0x3022af.toString().search("(((.+)+)+)+$").toString().constructor(_0x3022af).search("(((.+)+)+)+$");
+});
+_0x3022af();
+const _0x15ea92 = function () {
+  const _0x61c6b1 = {
+    ZOfeS: "while (true) {}"
+  };
+  _0x61c6b1.cQejk = "counter";
+  _0x61c6b1.uzebR = function (_0x49ff43, _0x22ea8f) {
+    return _0x49ff43 === _0x22ea8f;
+  };
+  _0x61c6b1.OopxB = "RKFqE";
+  _0x61c6b1.yMeki = "YkcNG";
+  _0x61c6b1.EbFCl = function (_0x4b4364, _0x2b2379) {
+    return _0x4b4364 !== _0x2b2379;
+  };
+  _0x61c6b1.VLFft = "zwVWV";
+  _0x61c6b1.TrduP = "fmEHP";
+  let _0x5a5c61 = true;
+  return function (_0x56a69c, _0x3e54c5) {
+    if (_0x61c6b1.EbFCl(_0x61c6b1.VLFft, _0x61c6b1.TrduP)) {
+      const _0x44ff20 = _0x5a5c61 ? function () {
+        const _0x280141 = {
+          WlMbq: "while (true) {}",
+          eJkIi: _0x61c6b1.cQejk
+        };
+        if (_0x61c6b1.uzebR(_0x61c6b1.OopxB, _0x61c6b1.OopxB)) {
+          if (_0x3e54c5) {
+            if (_0x61c6b1.uzebR(_0x61c6b1.yMeki, _0x61c6b1.yMeki)) {
+              const _0x34b61f = _0x3e54c5.apply(_0x56a69c, arguments);
+              _0x3e54c5 = null;
+              return _0x34b61f;
+            } else {
+              const _0x31e41d = _0x26c7cc ? function () {
+                if (_0x5d9db0) {
+                  const _0x51f709 = _0x5551ab.apply(_0xbb0d16, arguments);
+                  _0x2a19ef = null;
+                  return _0x51f709;
+                }
+              } : function () {};
+              _0x35ab49 = false;
+              return _0x31e41d;
+            }
+          }
+        } else {
+          return function (_0x525be5) {}.constructor("while (true) {}").apply(_0x280141.eJkIi);
+        }
+      } : function () {};
+      _0x5a5c61 = false;
+      return _0x44ff20;
+    } else {
+      return true;
+    }
+  };
+}();
+(function () {
+  let _0x1f6278;
+  try {
+    const _0xa9955d = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x1f6278 = _0xa9955d();
+  } catch (_0x51de86) {
+    _0x1f6278 = window;
+  }
+  _0x1f6278.setInterval(_0x57dc69, 4000);
+})();
+(function () {
+  _0x15ea92(this, function () {
+    const _0x596101 = new RegExp("function *\\( *\\)");
+    const _0x96d223 = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", 'i');
+    const _0x340308 = _0x57dc69("init");
+    if (!_0x596101.test(_0x340308 + "chain") || !_0x96d223.test(_0x340308 + "input")) {
+      _0x340308('0');
+    } else {
+      _0x57dc69();
+    }
+  })();
+})();
+const _0x9cafa0 = function () {
+  let _0x3edf91 = true;
+  return function (_0x71818f, _0x2ef225) {
+    const _0x5aeae8 = _0x3edf91 ? function () {
+      if (_0x2ef225) {
+        const _0x4ae71d = _0x2ef225.apply(_0x71818f, arguments);
+        _0x2ef225 = null;
+        return _0x4ae71d;
+      }
+    } : function () {};
+    _0x3edf91 = false;
+    return _0x5aeae8;
+  };
+}();
+const _0x4c177d = _0x9cafa0(this, function () {
+  let _0x1a0fa6;
+  try {
+    const _0x30ca96 = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x1a0fa6 = _0x30ca96();
+  } catch (_0x6e8a09) {
+    _0x1a0fa6 = window;
+  }
+  const _0x2f95a6 = _0x1a0fa6.console = _0x1a0fa6.console || {};
+  const _0x2739cc = ["log", "warn", "info", "error", "exception", "table", "trace"];
+  for (let _0x4b680a = 0; _0x4b680a < _0x2739cc.length; _0x4b680a++) {
+    const _0xf67625 = _0x9cafa0.constructor.prototype.bind(_0x9cafa0);
+    const _0x296966 = _0x2739cc[_0x4b680a];
+    const _0x4271d4 = _0x2f95a6[_0x296966] || _0xf67625;
+    _0xf67625.__proto__ = _0x9cafa0.bind(_0x9cafa0);
+    _0xf67625.toString = _0x4271d4.toString.bind(_0x4271d4);
+    _0x2f95a6[_0x296966] = _0xf67625;
+  }
+});
+_0x4c177d();
+const {
+  bravin 
+} = require("../bravin");
+const _0x1589e0 = {
+  flag: "🇺🇸",
+  country: "United States"
+};
+const _0xc10d5e = {
+  flag: "🇬🇧",
+  country: "United Kingdom"
+};
+const _0x35baea = {
+  flag: "🇫🇷",
+  country: "France"
+};
+const _0x5f4c12 = {
+  flag: "🇩🇪",
+  country: "Germany"
+};
+const _0x2fa79b = {
+  flag: "🇨🇦",
+  country: "Canada"
+};
+const _0x3652d3 = {
+  flag: "🇦🇺",
+  country: "Australia"
+};
+const _0x4d5ffc = {
+  flag: "🇯🇵",
+  country: "Japan"
+};
+const _0x1aba22 = {
+  flag: "🇮🇳",
+  country: "India"
+};
+const _0x455fda = {
+  flag: "🇮🇹",
+  country: "Italy"
+};
+const _0x4a9ba3 = {
+  flag: "🇪🇸",
+  country: "Spain"
+};
+const _0x330ee8 = {
+  flag: "🇷🇺",
+  country: "Russia"
+};
+const _0x4fd5ab = {
+  flag: "🇧🇷",
+  country: "Brazil"
+};
+const _0x2ea9b6 = {
+  flag: "🇹🇿",
+  country: "Tanzania"
+};
+const _0x233454 = {
+  flag: "🇲🇽",
+  country: "Mexico"
+};
+const _0x35683e = {
+  flag: "🇿🇦",
+  country: "South Africa"
+};
+const _0x25d9fd = {
+  flag: "🇰🇷",
+  country: "South Korea"
+};
+const _0x568ce0 = {
+  flag: "🇸🇦",
+  country: "Saudi Arabia"
+};
+const _0x7de683 = {
+  flag: "🇸🇬",
+  country: "Singapore"
+};
+const _0x1abd3c = {
+  flag: "🇸🇪",
+  country: "Sweden"
+};
+const _0x596035 = {
+  flag: "🇨🇭",
+  country: "Switzerland"
+};
+const _0x106703 = {
+  flag: "🇦🇷",
+  country: "Argentina"
+};
+const _0x2ede4c = {
+  flag: "🇦🇹",
+  country: "Austria"
+};
+const _0x5b49b9 = {
+  flag: "🇧🇪",
+  country: "Belgium"
+};
+const _0x357bfd = {
+  flag: "🇨🇱",
+  country: "Chile"
+};
+const _0x57de1e = {
+  flag: "🇨🇴",
+  country: "Colombia"
+};
+const _0x53869b = {
+  flag: "🇨🇿"
+};
+function _0x26f1eb(_0x1972a9, _0xc9a3f0, _0x5a021f, _0x59b8fb, _0x589e12) {
+  return _0x539d(_0x59b8fb + 0x5e, _0xc9a3f0);
+}
+_0x53869b.country = "Czech Republic";
+const _0x5dc56f = {
+  flag: "🇩🇰",
+  country: "Denmark"
+};
+const _0x4beaa8 = {
+  flag: "🇪🇬",
+  country: "Egypt"
+};
+const _0x2199b8 = {
+  flag: "🇫🇮",
+  country: "Finland"
+};
+const _0x577c9d = {
+  flag: "🇬🇷",
+  country: "Greece"
+};
+const _0x598c17 = {
+  flag: "🇭🇺",
+  country: "Hungary"
+};
+const _0x5929ca = {};
+function _0x44993e(_0x4f1b6d, _0xb6e030, _0x2dfabb, _0x495b5e, _0x4dc010) {
+  return _0x539d(_0x4f1b6d + 0x29f, _0x2dfabb);
+}
+_0x5929ca.flag = "🇮🇩";
+_0x5929ca.country = "Indonesia";
+const _0x527861 = {
+  flag: "🇮🇪",
+  country: "Ireland"
+};
+const _0x48c7b6 = {
+  flag: "🇮🇱",
+  country: "Israel"
+};
+const _0x1730c3 = {};
+function _0x587eac(_0x598031, _0x2673b7, _0x27b13c, _0x57ef5e, _0x5b327a) {
+  return _0x539d(_0x27b13c + 0x21, _0x2673b7);
+}
+_0x1730c3.flag = "🇯🇴";
+_0x1730c3.country = "Jordan";
+const _0x583876 = {
+  flag: "🇰🇿",
+  country: "Kazakhstan"
+};
+const _0x573791 = {
+  flag: "🇱🇧",
+  country: "Lebanon"
+};
+const _0x3b69ef = {
+  flag: "🇲🇾",
+  country: "Malaysia"
+};
+const _0xef7bf = {
+  flag: "🇲🇻",
+  country: "Maldives"
+};
+const _0x465714 = {
+  flag: "🇳🇱",
+  country: "Netherlands"
+};
+const _0x18fffd = {};
+function _0xa912eb(_0x150812, _0x2182c7, _0x3f823a, _0x40bf09, _0x27f9c7) {
+  return _0x539d(_0x40bf09 + 0xf, _0x150812);
+}
+_0x18fffd.flag = "🇳🇿";
+_0x18fffd.country = "New Zealand";
+const _0x1225ba = {
+  flag: "🇳🇴",
+  country: "Norway"
+};
+const _0x5804fe = {};
+function _0x213a89(_0x34ba4d, _0x2eb6f7, _0x113386, _0x477ce5, _0x17b30a) {
+  return _0x539d(_0x477ce5 + 0x248, _0x113386);
+}
+_0x5804fe.flag = "🇵🇰";
+_0x5804fe.country = "Pakistan";
+const _0x3458f2 = {
+  flag: "🇵🇱",
+  country: "Poland"
+};
+const _0x2b1bf5 = {
+  flag: "🇵🇹",
+  country: "Portugal"
+};
+const _0x46b1f4 = {};
+function _0x7635() {
+  const _0x1b5d9c = ['QWnQU', '🇵🇰', 'warn', 'vGgaT', 'zhrUW', 'EeYVs', 'KifWY', 'zEhWM', 'guess', "s fla", 'searc', 'pore', 'lengt', 'rando', 'bind', 'idTHk', 'sia', 'aEtSK', 'TrduP', '🇱🇧', 'Frfkv', 'DjnJb', 'yJyac', '🇷🇸', '🇸🇬', 'Japan', 'faIoA', 'NHAQL', '🇨🇭', "k abo", 'OGaro', 'Anuxw', 'init', 'debu', 'Games', "e 30 ", 'GWSzc', 'yMeki', 'Unite', " thin", " Afri", 'error', '🇦🇺', 'cZZdj', 'jUhjW', 'hrUoh', 'tes', 'NOfPl', 'BqvSw', 'PFyjE', 'PZXrL', 'SgFAj', 'iBdQm', 'CYNDe', 'NRZyO', 'pPHkd', 'PGLBn', 'Mqvxr', 'kou', 'essag', 'SBfOi', 'while', '🇷🇺', 'GUQzq', 'table', 'BONIPHACE', 'alia', 'CSpOU', '8qAXBpv', 'toStr', 'setIn', 'OopxB', 'IbbAC', 'LTmQr', 'stvNG', 'Polan', '🇦🇹', 'hONFJ', 'Nethe', 'bXWbo', 'LQKui', 'LItqD', "ry fo", 'OfgUX', 'ckVtp', 'yUOVT', '🇨🇴', 'TuQCm', 'aENNP', 'NvBaI', "rn th", 'bia', 'state', 'Colom', 'Irela', "D BY ", 'EwUOr', 'XRlrh', 'Pqfty', 'JaXmp', '🇲🇻', 'TWXME', '🇨🇦', 'lTwoc', 'mkXGs', 'WbzAz', 'XSHMG', "n (fu", 'EsLxS', " (tru", 'Denma', 'flxNb', 'chain', 'tion', 'Saudi', '🇫🇮', 'fmEHP', 'sAwnw', 'fvpnO', 'lzYds', 'hcYuB', 'AiBmc', 'rk/zo', 'sendM', '🇯🇵', 'mJbQP', 'India', 'TdjWc', '🇮🇳', 'GhQgq', 'Egypt', 'oKbyW', 'nomCo', 'cdtkn', 'Greec', 'Qatar', 'vzFSe', 'log', 'tina', 'QtaXc', 'Russi', 'mbBTi', 'pqGLf', 'wFtEf', 'PYSgE', 'uzebR', 'WlMbq', '🇪🇬', "ion *", 'vJmEZ', 'QPolr', 'ednIA', 'YkcNG', 'CHNEv', '__pro', 'Roman', 'RwONS', 'nswer', 'zA-Z_', 'nKbJh', 'Belgi', 'ZOfeS', 'text', 'IcCHN', 'QuyvP', 'LFMza', 'ZjVbJ', 'wnHmo', 'xXzob', 'GVjSw', '🇰🇷', 'qzqnQ', 'Singa', 'vhtYG', '🇭🇺', '858111rpyVJQ', 'actio', 'blic', 'CgSFc', '20746660VsFVKo', '🇦🇷', 'QAPXn', 'xvrzK', 'hNtGs', 'AqjtP', 'Spain', 'dDVwm', 'OixZv', ".\n> P", 'xIROd', 'lbMly', 'NCIec', 'wBFNH', 'VNZrF', '{}.co', " was:", 'QuGbf', 'Guess', 'fUTtj', 'XNeMU', 'strin', '🇸🇦', 'tpPWl', 'SzVIB', 'Austr', 'KpxZo', 'orie', 'tVzYS', 'vtfnh', 'funct', "The a", 'eLOZM', 'bVnBj', 'WeTxk', '🇮🇪', 'DQpdb', 'ivHbI', "g: ", "r thi", 'apPrX', 'Serbi', 'CTpdC', 'Italy', '🇲🇽', 'Brazi', "\\+\\+ ", 'YmPBO', 'qJPZS', '(((.+', 'cCShM', 'gyhbr', 'TvPlG', "u hav", 'Switz', 'UUehD', 'xEhgv', 'XRTmJ', '🇶🇦', 'UFQJN', 'Czech', 'gdom', '*(?:[', 'categ', 'BFYSZ', 'aalgo', 'Objec', 'EbFCl', 'ukKMA', 'QsyDv', 'HVTyS', 'nryeS', 'RJnEE', '$]*)', '132tAnRCS', 'GIOqe', '🇳🇱', '1113155iMEzFy', 'esia', 'Swede', '1ePBJmO', 'AbRXJ', 'gObPy', 'secon', 'DXYze', 'JvXKf', 'call', 'excep', 'IgQPE', '🇿🇦', " Arab", 'Hawtd', "d Kin", '🇩🇰', 'Ulhaq', 'Norwa', '_MD', 'KiDSu', '🇹🇿', "ds to", 'OhJgY', 'vvRCG', 'GXRxA', 'kFmfv', 'WYuXF', 'zJydK', '🇬🇷', 'Leban', 'Portu', 'apply', 'lYOTT', 'a-zA-', " Repu", 'ves', 'YVGqB', 'Canad', 'IViFo', 'yrHKh', 'Vzbsk', 'iALyq', 'XSdKA', 'Pakis', 'cQejk', '🇮🇩', 'RKFqE', 'WOUdQ', 'input', 'gIWQS', 'DRzRI', 'EdBte', 'ixehW', 'rsRSu', 'FdwLv', 'hvoWm', '🇷🇴', 'Maldi', 'KbXni', 'DvrcH', 'mJQpv', "New Z", 'terva', 'OUlCR', 'exzQv', 'yzxNZ', 'eJkIi', '🇧🇪', ')+)+)', 'DdzYG', 'nEuST', 'Indon', 'wPQKQ', '🇧🇷', 'South', 'sbQIQ', 'Bpcnz', 'kXPLC', 'jmbDt', 'cYhyx', 'OWERE', 'fwOGc', 'ealan', 'Israe', 'KzCYv', '🇨🇱', 'ing', 'nCSgE', '🇵🇹', 'proto', 'Hunga', 'yDUvg', 'floor', '🇲🇾', '🇸🇪', "d Sta", 'hstan', 'ructo', 'tan', 'VjFzM', 'tNzXV', 'erlan', '🇰🇿', 'rPFTB', 'TuoPt', 'flag', 'ZYytW', 'Z_$][', '🇵🇱', 'Ahvob', 'DmBgP', 'trace', 'raocy', "n() ", '🇯🇴', 'ujHOn', "e) {}", '1475440CEFJvt', 'rland', '🇮🇹', 'cDvPS', 'hMbkz', '🇬🇧', 'Mexic', 'XqoQX', 'nOgRa', '🇩🇪', 'PoOcK', 'sxwEB', 'count', 'shoRU', 'BgNWY', 'const', ". \nYo", 'retur', 'Finla', '🇮🇱', 'YVMms', 'LbLIw', '🇳🇿', 'jkwCz', 'PIuwG', 'Kazak', 'nSzxy', 'conso', 'wCJpD', '../fr', 'to__', 'VHNfu', 'gal', 'info', 'pzXbR', 'TyUwd', '65975poqGan', '🇺🇸', 'Malay', 'YlPWE', 'eEmxi', '🇳🇴', 'ksrLi', 'nctio', 'KQgaw', 'YwDfd', 'XTUCd', 'HFuhu', 'FvRwk', 'quote', 'UMFNJ', 'Franc', 'lUUid', 'EHqyL', '🇨🇿', "\"retu", 'lIDbQ', "is\")(", 'Chile', 'WusIq', '0-9a-', 'GXPvd', 'UEUrH', 'YmNlV', 'nstru', 'eRqjy', 'gger', 'XcRSY', " the ", 'type', 'VLFft', 'Lzutm', 'cyblK', 'trFzf', 'BdkJm', 'RaBCr', 'ggXDo', '1311114KwiKKf', 'YrrZM', 'mniTR', "\\( *\\", 'XdAmM', 'EqnKP', 'Jorda', 'Tanzania', 'test', '🇫🇷', 'IXLxv', 'iXjaQ', 'TwgTy', '4017240yItzww', 'VEBxh', " Kore", 'Germa', 'GSrjG', '9CiApXg', 'FSCZf', 'QBsza', '🇪🇸', "ut it", 'CZeCW', 'IuEOe', 'tdGKR', 'Argen', 'amewo', 'JcNXK', 'ctor(', 'OUUAO', 'zwVWV', 'mKSIq'];
+  _0x7635 = function () {
+    return _0x1b5d9c;
+  };
+  return _0x7635();
+}
+_0x46b1f4.flag = "🇶🇦";
+_0x46b1f4.country = "Qatar";
+const _0x5c5592 = {
+  flag: "🇷🇴",
+  country: "Romania"
+};
+const _0x3771b6 = {
+  flag: "🇷🇸",
+  country: "Serbia"
+};
+const flagPairs = [_0x1589e0, _0xc10d5e, _0x35baea, _0x5f4c12, _0x2fa79b, _0x3652d3, _0x4d5ffc, _0x1aba22, _0x455fda, _0x4a9ba3, _0x330ee8, _0x4fd5ab, _0x2ea9b6, _0x233454, _0x35683e, _0x25d9fd, _0x568ce0, _0x7de683, _0x1abd3c, _0x596035, _0x106703, _0x2ede4c, _0x5b49b9, _0x357bfd, _0x57de1e, _0x53869b, _0x5dc56f, _0x4beaa8, _0x2199b8, _0x577c9d, _0x598c17, _0x5929ca, _0x527861, _0x48c7b6, _0x1730c3, _0x583876, _0x573791, _0x3b69ef, _0xef7bf, _0x465714, _0x18fffd, _0x1225ba, _0x5804fe, _0x3458f2, _0x2b1bf5, _0x46b1f4, _0x5c5592, _0x3771b6];
+const _0x133970 = {
+  nomCom: "guessflag",
+  categorie: "Games"
+};
+bravin (_0x133970, async (_0x5f3dd2, _0x56c8a7, _0x46641d) => {
+  const {
+    ms: _0x605086,
+    repondre: _0x4c86c0
+  } = _0x46641d;
+  const _0x51e4c1 = flagPairs[Math.floor(Math.random() * flagPairs.length)];
+  const _0x332920 = {
+    text: "Guess the country for this flag: " + _0x51e4c1.flag + ". \nYou have 30 seconds to think about it.\n> POWERED BY BONIPHACE_MD"
+  };
+  const _0x51c9ed = {
+    quoted: _0x605086
+  };
+  await _0x56c8a7.sendMessage(_0x5f3dd2, _0x332920, _0x51c9ed);
+  await delay(30000);
+  const _0x431ed2 = {
+    text: "The answer was: " + _0x51e4c1.country + '.'
+  };
+  const _0x317805 = {
+    quoted: _0x605086
+  };
+  await _0x56c8a7.sendMessage(_0x5f3dd2, _0x431ed2, _0x317805);
+});
+function delay(_0x2f48cf) {
+  return new Promise(_0x169cfe => setTimeout(_0x169cfe, _0x2f48cf));
+}
+function _0x57dc69(_0x1351bc) {
+  function _0x36fafa(_0x53a140) {
+    if (typeof _0x53a140 === "string") {
+      return function (_0x4595ff) {}.constructor("while (true) {}").apply("counter");
+    } else {
+      if (('' + _0x53a140 / _0x53a140).length !== 1 || _0x53a140 % 20 === 0) {
+        (function () {
+          return true;
+        }).constructor("debugger").call("action");
+      } else {
+        (function () {
+          return false;
+        }).constructor("debugger").apply("stateObject");
+      }
+    }
+    _0x36fafa(++_0x53a140);
+  }
+  try {
+    if (_0x1351bc) {
+      return _0x36fafa;
+    } else {
+      _0x36fafa(0);
+    }
+  } catch (_0x471d50) {}
+  }
