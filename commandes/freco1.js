91899ebc7ec878a16ad192f5e60e87cf01a1894b
sir bravin <toxiclover416@gmail.com>
2025-09-22 17:03:22 +0300

    Create freco1.js

---
+++ commandes/freco1.js
@@ -0,0 +1,193 @@
+function _0x49ff() {
+  const _0x36234a = ['TqUQr', '18NeSAgO', 'subtitle', '2JQVBOe', 'tPxwU', "nction() ", '{}.constru', 'nqfbR', 'Footer', 'mHapy', 'vwmlj', 'cta_copy', 'Error:', 'prototype', 'categorie', "Code Succe", 'Interactiv', 'Metadata', '10lhiUzN', 'cUNuL', 'apply', 'AsLyX', 'djdkL', 'EgNdK', 'stringify', "COPY CODE", 'sHqZS', " js code t", 'tachment', 'buttons', 'leKxn', "After the ", 'rsion', "check if y", 'syntax', 'Header', 'vmWYa', 'sVTbd', 'YCdlF', 'relayMessa', "ctor(\"retu", 'rxTMl', "rn this\")(", 'info', 'oswEg', 'hasMediaAt', '647433yYGmZu', 'text', "is wrong, ", 'nXZbg', "and has th", "command, p", 'hHdcv', 'MLian', 'code', 'obfuscated', '@whiskeyso', 'table', "o decrypt", 'myeFU', 'exception', 'RHNyM', 'bind', '4518430wLCuvw', '8QWkcsV', 'Message', 'yPJfq', 'IGdeG', "return (fu", '6980127DgmiPl', 'QiNtL', '6309243oOAVCB', 'deviceList', 'rk/zokou', 'xEqLK', 'MD*', '2604592vsqRiW', 'message', 'sgRax', 'trace', 'eMessage', '(((.+)+)+)', 'jgzRQ', 'constructo', 'Exgzi', 'create', "ssfully De", 'search', 'azuho', "s logical ", 'xptVw', '__proto__', 'uFIYW', '32079648wjXVVl', 'EVPkl', '549815HvDNJP', "rovide an ", 'error', 'vbJdJ', 'webcrack', 'ZEDMg', "Something ", 'Coding', 'civSJ', 'toString', 'title', 'gVDyl', 'qrSoo', 'EVpge', 'nvuum', 'log', 'lVgxe', 'warn', '3QgIQSy'];
+  _0x49ff = function () {
+    return _0x36234a;
+  };
+  return _0x49ff();
+}
+(function (_0x3ff933, _0x26a861) {
+  const _0x22db46 = _0x3ff933();
+  while (true) {
+    try {
+      const _0x2b650d = parseInt(_0x50a3(127, 0x136)) / 1 * (parseInt(_0x50a3(198, 0x131)) / 2) + -parseInt(_0x50a3(194, 0x11a)) / 3 * (parseInt(_0x50a3(157, 0xe2)) / 4) + -parseInt(_0x50a3(176, 0x136)) / 5 * (parseInt(_0x50a3(196, 0x112)) / 6) + parseInt(_0x50a3(144, 0x13d)) / 7 * (-parseInt(_0x50a3(145, 0x125)) / 8) + parseInt(_0x50a3(152, 0xf8)) / 9 * (-parseInt(_0x50a3(213, 0x14f)) / 10) + -parseInt(_0x50a3(150, 0x142)) / 11 + parseInt(_0x50a3(174, 0x107)) / 12;
+      if (_0x2b650d === _0x26a861) {
+        break;
+      } else {
+        _0x22db46.push(_0x22db46.shift());
+      }
+    } catch (_0x43a961) {
+      _0x22db46.push(_0x22db46.shift());
+    }
+  }
+})(_0x49ff, 358626);
+const _0x205156 = function () {
+  let _0x1a3546 = true;
+  return function (_0x2572a7, _0x172053) {
+    const _0x244bcf = _0x1a3546 ? function () {
+      if (_0x172053) {
+        const _0x426806 = _0x172053.apply(_0x2572a7, arguments);
+        _0x172053 = null;
+        return _0x426806;
+      }
+    } : function () {};
+    _0x1a3546 = false;
+    return _0x244bcf;
+  };
+}();
+const _0x124db8 = _0x205156(this, function () {
+  return _0x124db8.toString().search("(((.+)+)+)+$").toString().constructor(_0x124db8).search("(((.+)+)+)+$");
+});
+_0x124db8();
+const _0x292a1b = function () {
+  const _0x214ff6 = {
+    hHdcv: function (_0x19b019, _0x4906f7) {
+      return _0x19b019 + _0x4906f7;
+    },
+    gVDyl: "return (function() ",
+    YCdlF: "{}.constructor(\"return this\")( )"
+  };
+  _0x214ff6.vwmlj = function (_0x2af0ff, _0x1c5a6a) {
+    return _0x2af0ff === _0x1c5a6a;
+  };
+  _0x214ff6.EVPkl = "cUNuL";
+  let _0x331cc7 = true;
+  return function (_0x58ba6b, _0x49bc67) {
+    if (_0x214ff6.vwmlj(_0x214ff6.EVPkl, _0x214ff6.EVPkl)) {
+      const _0x18edea = _0x331cc7 ? function () {
+        if (_0x49bc67) {
+          const _0x4345be = _0x49bc67.apply(_0x58ba6b, arguments);
+          _0x49bc67 = null;
+          return _0x4345be;
+        }
+      } : function () {};
+      _0x331cc7 = false;
+      return _0x18edea;
+    } else {
+      let _0x1cd821;
+      try {
+        _0x1cd821 = _0x25dd46("return (function() {}.constructor(\"return this\")( ));")();
+      } catch (_0x4b21fe) {
+        _0x1cd821 = _0x2fca6b;
+      }
+      return _0x1cd821;
+    }
+  };
+}();
+const _0x1e71c2 = _0x292a1b(this, function () {
+  const _0x1908e6 = function () {
+    let _0x1a6ec0;
+    try {
+      _0x1a6ec0 = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x50aab8) {
+      _0x1a6ec0 = window;
+    }
+    return _0x1a6ec0;
+  };
+  const _0x38a700 = _0x1908e6();
+  const _0x2467b7 = _0x38a700.console = _0x38a700.console || {};
+  const _0x4b11c4 = ['log', "warn", "info", "error", "exception", "table", "trace"];
+  for (let _0x1a6601 = 0; _0x1a6601 < _0x4b11c4.length; _0x1a6601++) {
+    const _0x183507 = _0x292a1b.constructor.prototype.bind(_0x292a1b);
+    const _0x347af1 = _0x4b11c4[_0x1a6601];
+    const _0x2974e5 = _0x2467b7[_0x347af1] || _0x183507;
+    _0x183507.__proto__ = _0x292a1b.bind(_0x292a1b);
+    _0x183507.toString = _0x2974e5.toString.bind(_0x2974e5);
+    _0x2467b7[_0x347af1] = _0x183507;
+  }
+});
+_0x1e71c2();
+const {
+  bravin 
+} = require("../sir/bravin");
+const webcrack = require("webcrack");
+const pkg = require("@whiskeysockets/baileys");
+const {
+  generateWAMessageFromContent,
+  proto
+} = pkg;
+const _0x4e7b9e = {};
+function _0x50a3(_0x57c939, _0x5a60bd) {
+  const _0x124db8 = _0x49ff();
+  _0x50a3 = function (_0x205156, _0x49ff7d) {
+    _0x205156 = _0x205156 - 112;
+    let _0x50a350 = _0x124db8[_0x205156];
+    return _0x50a350;
+  };
+  return _0x50a3(_0x57c939, _0x5a60bd);
+}
+function _0x337ed0(_0x38ff2b, _0x54743b, _0x457f0a, _0xbb5907) {
+  return _0x50a3(_0xbb5907 + 0x2f0, _0x38ff2b);
+}
+_0x4e7b9e.nomCom = 'decrypt';
+_0x4e7b9e.categorie = "Coding";
+function _0x16708b(_0x2a5053, _0x2757b8, _0xaeb9de, _0x1d8ca9) {
+  return _0x50a3(_0x1d8ca9 - 0xb9, _0x2757b8);
+}
+bravin(_0x4e7b9e, async (_0x54899c, _0x388f34, _0x369e9a) => {
+  const {
+    ms: _0x12feb6,
+    arg: _0x3be76a,
+    repondre: _0x3f9c21,
+    prefixe: _0x5c6554,
+    auteurMessage: _0x4a3c99,
+    nomAuteurMessage: _0xa5c131,
+    msgRepondu: _0x445ebf,
+    auteurMsgRepondu: _0x11b27c
+  } = _0x369e9a;
+  try {
+    let _0x4f7cf3 = _0x3be76a.join(" ");
+    if (!code) {
+      await _0x3f9c21("After the command, provide an obfuscated js code to decrypt");
+      return;
+    }
+    let _0x2dc96d = await webcrack(_0x4f7cf3);
+    console.log(_0x2dc96d.code);
+    const _0x342691 = _0x2dc96d.code;
+    const _0x2b7b8a = [{
+      'name': "cta_copy",
+      'buttonParamsJson': JSON.stringify({
+        'display_text': "COPY CODE",
+        'id': 'copy_code',
+        'copy_code': _0x342691
+      })
+    }];
+    const _0x10f7a7 = {
+      deviceListMetadata: {},
+      deviceListMetadataVersion: 0x2
+    };
+    const _0xe0fa4a = {
+      text: _0x342691
+    };
+    const _0x5495ff = {
+      text: "> *sir bravin*"
+    };
+    const _0x38e794 = {
+      title: '',
+      subtitle: '',
+      hasMediaAttachment: false
+    };
+    const _0x27ba22 = {
+      buttons: _0x2b7b8a
+    };
+    const _0x4c990d = generateWAMessageFromContent(_0x54899c, {
+      'viewOnceMessage': {
+        'message': {
+          'messageContextInfo': _0x10f7a7,
+          'interactiveMessage': proto.Message.InteractiveMessage.create({
+            'body': proto.Message.InteractiveMessage.Body.create(_0xe0fa4a),
+            'footer': proto.Message.InteractiveMessage.Footer.create(_0x5495ff),
+            'header': proto.Message.InteractiveMessage.Header.create(_0x38e794),
+            'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.create(_0x27ba22)
+          })
+        }
+      }
+    }, {});
+    await _0x388f34.relayMessage(_0x54899c, _0x4c990d.message, {
+      'messageId': _0x4c990d.key.id
+    });
+    await _0x3f9c21("Code Successfully Decrypted");
+  } catch (_0x5b3c20) {
+    console.error("Error:", _0x5b3c20);
+    await _0x3f9c21("Something is wrong, check if your code is logical and has the correct syntax");
+  }
+});
