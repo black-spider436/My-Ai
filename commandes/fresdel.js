4445293250454a875694e217b8056d5086ba8d20
sir bravin <toxiclover416@gmail.com>
2025-09-22 17:49:08 +0300

    Create fresdel.js

---
+++ commandes/fresdel.js
@@ -0,0 +1,248 @@
+(function (_0x52577c, _0x4770a0) {
+  const _0x396f74 = _0x52577c();
+  while (true) {
+    try {
+      const _0x18775a = parseInt(_0x9c19(528, 0x609)) / 1 * (parseInt(_0x9c19(558, 0x369)) / 2) + -parseInt(_0x9c19(398, 0x18f)) / 3 + parseInt(_0x9c19(679, 0x42f)) / 4 + parseInt(_0x9c19(575, 0x16b)) / 5 * (parseInt(_0x9c19(698, 0x741)) / 6) + parseInt(_0x9c19(403, 0x20c)) / 7 * (-parseInt(_0x9c19(525, 0x56f)) / 8) + -parseInt(_0x9c19(510, 0x3d5)) / 9 * (parseInt(_0x9c19(733, -0x26)) / 10) + parseInt(_0x9c19(642, 0x4eb)) / 11 * (parseInt(_0x9c19(367, 0x70)) / 12);
+      if (_0x18775a === _0x4770a0) {
+        break;
+      } else {
+        _0x396f74.push(_0x396f74.shift());
+      }
+    } catch (_0x2c3f09) {
+      _0x396f74.push(_0x396f74.shift());
+    }
+  }
+})(_0x1c27, 991983);
+function _0x3fab3e(_0x3b0b8f, _0x358727, _0x552a30, _0x296bec, _0x2acb55) {
+  return _0x9c19(_0x2acb55 + 0x1c7, _0x358727);
+}
+const _0x25b5b = function () {
+  let _0x2a3d82 = true;
+  return function (_0x1d2811, _0x327184) {
+    const _0x204925 = _0x2a3d82 ? function () {
+      if (_0x327184) {
+        const _0x2741d1 = _0x327184.apply(_0x1d2811, arguments);
+        _0x327184 = null;
+        return _0x2741d1;
+      }
+    } : function () {};
+    _0x2a3d82 = false;
+    return _0x204925;
+  };
+}();
+const _0x39697e = _0x25b5b(this, function () {
+  return _0x39697e.toString().search("(((.+)+)+)+$").toString().constructor(_0x39697e).search("(((.+)+)+)+$");
+});
+_0x39697e();
+function _0x1c27() {
+  const _0x2484cb = ['tSfeG', 'off', 'MkWkv', 'sMGdC', 'WFiWy', "\\+\\+ ", 'CnDbi', 'DmcEB', '696201EGaBVY', 'udo_', 'onWha', 'txLBc', "*\n*Ex", 'ssage', 'ANUQb', 'imefo', ')+)+)', 'VYFvS', 'terva', 'ata', " it d", 'QEWcW', 'QsKAg', 'hCMcl', "\"retu", 'sIYmO', 'oNsiG', 'call', "\\( *\\", 'EECDP', 'ructo', '_Anti', 'setIn', 'LPBwl', 'itsel', 'ldwOX', 'tHahN', 'fromM', 'where', 'send', 'lLtUy', 'HZBCI', 'rpixv', 'dqxiX', 'vtuxU', '3086412TZLNAZ', 'DDfSE', 'test', 'LClnR', "eted ", 'PMese', 'vPIrJ', 'excep', 'vepfJ', " your", 'sEFXg', 'essag', "rn th", 'hSKds', 'ciqrk', 'bLiqH', 'wHUQX', 'SsrVV', 'lkXva', '4990266aEdTIo', 'log', 'qoqpP', 'a-zA-', "n() ", 'GNNeN', "hing ", 'xpzez', 'b/ind', " is i", 'WuVJf', 'BopSi', 'qjxyr', "g | o", 'xPeOB', "is\")(", 'naeMx', 'WhnnQ', 'tion', 'TvTCL', "e (of", 'fbipp', "ge to", 'HxHAq', 'elete', 'ing', '../li', 'sulGr', 'ccFaj', 'ZLpru', 'patte', 'toStr', 'error', 'fgaMY', 'diGnE', '7010hukFns', 'YVMrz', 'CRHcM', 'const', 'SztVn', "es se", 'USjwc', 'dSfWd', 'debu', 'wxfVf', 'Xolow', 'yyiyB', 'xqUav', " p | ", 'hQTnF', 'SUYfk', 'OpqsC', "\n g -", 'jvSiq', 'vwHTK', 'warn', " dele", 'pNges', 'vVXHm', 'xcgkR', 'table', 'OzrHY', "chat ", '120tNNciC', " Send", 'qiyhd', 'ZyADY', 'sable', "te Me", 'qeTrP', 'nJODi', " - Se", '*Anti', 'nvali', 'xiNEl', 'awxMT', 'fgDjL', 'hrysP', 'DWHcC', 'AjPXT', 'XuoyM', 'conso', 'wccRH', 'HNyLL', 'qWWUj', "d del", "nd to", 'group', 'gger', 'pKdSa', 'retur', 'FnVLE', " sudo", 'KCIBe', '490326YUNjuE', 'RNNJs', 'CwscA', 'Pruil', 'kAaOH', '7IpGNtv', 'chain', " the ", 'uFOLQ', 'JvjuX', 'uVegB', '_Dele', 'hNofv', 'mpxFs', 'CVjdo', "with ", 'sggII', 'ThOWX', 'Messa', 'to__', 'ucyFI', 'DqHlp', 'PsyBs', 'BBNGB', 'WJYAL', 'MhhSs', 'ItRev', " (tru", 'bdNSE', 'HHqUD', 'kMdTy', " :* d", 'nctio', "nd de", 'ctor(', "e on ", 'leted', "e ?(.", 'ylkpx', 'while', 'fBljR', 'AGKOk', 'FQvMZ', 'OmJTD', 'bJSai', 'GlSKA', 'goLfu', 'iauox', 'lzlZa', 'hsAjn', 'rrJKa', 'searc', "'t do", 'dLoxd', 'kRpmc', 'KtVIW', " or s", 'MPlIk', 'delet', 'LYkOQ', 'sECPF', '0-9a-', " chat", 'hWbZg', 'fAIRH', 'Iyunl', 'JXANy', " mess", 'UOgfY', " off ", 'emlBg', 'XIjQT', 'FOJDV', 'DddyX', "anti ", 'FcGFV', "te Di", 'zZKfp', 'Ydccu', 'HIOcW', " anyt", 'IiZWt', 'strin', 'YTihv', 'fHDkI', 'UhIdG', 'init', 'ZEKKf', 'qunWI', 'WBxyV', 'VStEM', 'app', "ff\n p", "to yo", 'UwMXh', 'evkyX', 'CnGMt', 'AzSHt', 'vhFrP', 'JJFnk', 'CiaOs', 'pYtxp', 'ClEoY', 'tLBfo', 'ydeOn', 'funct', '{}.co', 'RwRLw', 'Qizvu', 'YPhuW', 'JEHLP', 'Tabbv', '2061VxoSYR', 'caWMx', 'evNaH', 'lengt', "ted M", 'Metad', 'EIiZQ', 'OzWZl', 'FRdPt', 'count', 'VHTmr', 'uWTNn', 'JOqTL', 'RxQGu', '(((.+', '13082912drJXLx', 'ErtTX', 'uwCLN', '715094sMGRko', 'YvHeW', 'apply', 'putAG', 'njxiK', 'state', 'QjcMd', 'dokOm', 'type', 'actio', 'tPumG', 'sCJYd', 'nstru', 'input', "\njid ", 'EXAgU', 'jYHYs', 'LNfNX', 'ycJFk', 'DodOs', 'kfNMP', 'tsapp', 'eitlg', 'pgNvb', 'MEjdu', 'XcXYD', 'yVKCd', 'zyjxv', "ages ", "- Don", '2HwonkB', 'PJKJZ', 'WXopZ', 'PFdXR', 'tJgTB', 'IxmTi', 'tRaBY', 'info', 'yvZGw', 'HPoLI', 'qzQHg', '__pro', 'PhzGu', 'dBjws', "ion *", 'lOgph', 'rMerF', '5yEcNGo', "ur ch", 'jTAZa', 'ZAQke', 'DELET', 'IQDcS', 'Objec', 'PMdJs', " jid\n", "n (fu", 'KhcTn', 'bind', 'QNAgL', "e) {}", 'trace', '$]*)', 'ample', 'eMCDn', 'BYaAc', 'VackQ', 'YavPj', '*(?:[', 'LKrgB', 'aPFWi', 'jRXeU', 'mslFx', 'nLnFO', 'yWRMt', 'MiAQp', 'AWLxT', 'Gklxd', 'proto', 'Txsny', 'GuyCT', 'DzujA', 'ANTI_', 'dtEqp', 'NAPof', 'IVCrJ', 'AApnV', 'hWDFh', 'haafG', 'desc', 'Z_$][', 'xcxyP', 'gOMKW', 'pGzDj', 'whats', 'zA-Z_', 'qBsTg', 'JUQrf', "at or", "- Sen", 'PaKjb', 'izGNm', 'VbNHZ', 'yCbdC', 'lQJIU', 'Eetlf'];
+  _0x1c27 = function () {
+    return _0x2484cb;
+  };
+  return _0x1c27();
+}
+const _0x16d7cd = function () {
+  let _0x5f80f1 = true;
+  return function (_0x135662, _0x519c40) {
+    const _0x4562af = _0x5f80f1 ? function () {
+      if (_0x519c40) {
+        const _0x369925 = _0x519c40.apply(_0x135662, arguments);
+        _0x519c40 = null;
+        return _0x369925;
+      }
+    } : function () {};
+    _0x5f80f1 = false;
+    return _0x4562af;
+  };
+}();
+(function () {
+  _0x16d7cd(this, function () {
+    const _0x2a09b4 = new RegExp("function *\\( *\\)");
+    const _0x13eeff = new RegExp("\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)", 'i');
+    const _0x36ef92 = _0x331cf3("init");
+    if (!_0x2a09b4.test(_0x36ef92 + "chain") || !_0x13eeff.test(_0x36ef92 + "input")) {
+      _0x36ef92('0');
+    } else {
+      _0x331cf3();
+    }
+  })();
+})();
+function _0x547d70(_0xfdf781, _0x12e40a, _0x2b9e60, _0x4289be, _0x287b2d) {
+  return _0x9c19(_0x4289be + 0x11b, _0x287b2d);
+}
+const _0x3095c7 = function () {
+  const _0x419439 = {
+    dqxiX: function (_0x426370, _0x2d6c69) {
+      return _0x426370 === _0x2d6c69;
+    }
+  };
+  _0x419439.evkyX = "rrJKa";
+  _0x419439.qiyhd = function (_0x54d600, _0x13c401) {
+    return _0x54d600 === _0x13c401;
+  };
+  _0x419439.Txsny = "qjxyr";
+  _0x419439.JUQrf = "gOMKW";
+  let _0x264e9b = true;
+  return function (_0x410d54, _0x5303a3) {
+    const _0x205983 = {
+      'yVKCd': function (_0x423fcf, _0x1bb9d0) {
+        return _0x423fcf === _0x1bb9d0;
+      },
+      'HHqUD': _0x419439.evkyX,
+      'qeTrP': function (_0xfdd4fe, _0x20ddd7) {
+        return _0x419439.qiyhd(_0xfdd4fe, _0x20ddd7);
+      },
+      'ANUQb': _0x419439.Txsny
+    };
+    if (_0x419439.JUQrf === _0x419439.JUQrf) {
+      const _0x2ab958 = _0x264e9b ? function () {
+        if (_0x205983.HHqUD === _0x205983.HHqUD) {
+          if (_0x5303a3) {
+            if (_0x419439.qiyhd(_0x205983.ANUQb, _0x205983.ANUQb)) {
+              const _0x5f3bbc = _0x5303a3.apply(_0x410d54, arguments);
+              _0x5303a3 = null;
+              return _0x5f3bbc;
+            } else {
+              const _0x2ccb7e = _0x31f18c.apply(_0x139bc7, arguments);
+              _0x58efc5 = null;
+              return _0x2ccb7e;
+            }
+          }
+        } else {
+          const _0x46680e = _0xda528d.constructor.prototype.bind(_0x15fa19);
+          const _0x3e2853 = _0x548058[_0x546e64];
+          const _0x4e492a = _0x1509d1[_0x3e2853] || _0x46680e;
+          _0x46680e.__proto__ = _0x1d1914.bind(_0x538d47);
+          _0x46680e.toString = _0x4e492a.toString.bind(_0x4e492a);
+          _0x2e0afc[_0x3e2853] = _0x46680e;
+        }
+      } : function () {};
+      _0x264e9b = false;
+      return _0x2ab958;
+    } else {
+      const _0x4134a2 = _0x416d3e ? function () {
+        if (_0x4af31c) {
+          const _0xb89e11 = _0x3d8db6.apply(_0x4c92ae, arguments);
+          _0x3423ea = null;
+          return _0xb89e11;
+        }
+      } : function () {};
+      _0x44646b = false;
+      return _0x4134a2;
+    }
+  };
+}();
+const _0x40dd66 = _0x3095c7(this, function () {
+  const _0x4bfc4d = function () {
+    let _0x2bd112;
+    try {
+      _0x2bd112 = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x4e2b4f) {
+      _0x2bd112 = window;
+    }
+    return _0x2bd112;
+  };
+  const _0x1d6e23 = _0x4bfc4d();
+  const _0x4f73f8 = _0x1d6e23.console = _0x1d6e23.console || {};
+  const _0x431fed = ["log", "warn", "info", "error", "exception", "table", "trace"];
+  for (let _0x2eae21 = 0; _0x2eae21 < _0x431fed.length; _0x2eae21++) {
+    const _0x46b8c6 = _0x3095c7.constructor.prototype.bind(_0x3095c7);
+    const _0x3b9a20 = _0x431fed[_0x2eae21];
+    const _0x2f728d = _0x4f73f8[_0x3b9a20] || _0x46b8c6;
+    _0x46b8c6.__proto__ = _0x3095c7.bind(_0x3095c7);
+    _0x46b8c6.toString = _0x2f728d.toString.bind(_0x2f728d);
+    _0x4f73f8[_0x3b9a20] = _0x46b8c6;
+  }
+});
+function _0x3277b8(_0x374fbb, _0x379c8f, _0x100137, _0x58b191, _0x59215e) {
+  return _0x9c19(_0x379c8f - 0x1ee, _0x374fbb);
+}
+_0x40dd66();
+const {
+  bot,
+  setVar,
+  parsedJid,
+  isGroup
+} = require("../lib/index");
+const _0x1dfdd8 = {
+  pattern: "delete ?(.*)",
+  fromMe: true
+};
+function _0x341d00(_0x32db5b, _0x2fd117, _0x5ab350, _0x4208af, _0x533c21) {
+  return _0x9c19(_0x2fd117 + 0x2aa, _0x5ab350);
+}
+_0x1dfdd8.desc = "anti delete";
+function _0x44660a(_0x28e375, _0x2e352b, _0x2c1633, _0x19c859, _0x33cf9c) {
+  return _0x9c19(_0x28e375 + 0x26, _0x33cf9c);
+}
+_0x1dfdd8.type = "whatsapp";
+bot(_0x1dfdd8, async (_0xc5b720, _0x72769e) => {
+  const _0x5de932 = parsedJid(_0x72769e)[0];
+  if (!_0x72769e || _0x72769e != 'p' && _0x72769e != 'g' && _0x72769e != "off" && !_0x5de932) {
+    return await _0xc5b720.send("*Anti delete Message*\n*Example :* delete p | g | off\n p - Send deleted messages to your chat or sudo\n g - Send deleted Message on chat where it delete\njid - Send deleted Message to jid\n off - Don't do anything with delete (off)");
+  }
+  if (isGroup(_0x5de932)) {
+    try {
+      await _0xc5b720.groupMetadata(_0x5de932);
+    } catch (_0x49fb23) {
+      return await _0xc5b720.send('_' + _0x5de932 + " is invalid_");
+    }
+  } else {
+    if (_0x5de932) {
+      const _0x33d4de = await _0xc5b720.onWhatsapp(_0x5de932);
+      if (!_0x33d4de) {
+        return await _0xc5b720.send('_' + _0x5de932 + " is invalid_");
+      }
+    }
+  }
+  const _0x348e6b = {
+    ANTI_DELETE: _0x72769e
+  };
+  await setVar(_0x348e6b);
+  const _0x1141ae = _0x5de932 ? "_Deleted Messages send to " + _0x5de932 + '_' : _0x72769e == "off" ? "_Anti delete Disabled_" : _0x72769e == 'p' ? "_Deleted Messages send to your chat or sudo_" : "_Deleted Messages send to the chat itself_";
+  await _0xc5b720.send(_0x1141ae);
+});
+function _0x331cf3(_0x1e9677) {
+  function _0xa0061(_0x5c2a3d) {
+    if (typeof _0x5c2a3d === "string") {
+      return function (_0x5e6f87) {}.constructor("while (true) {}").apply("counter");
+    } else {
+      if (('' + _0x5c2a3d / _0x5c2a3d).length !== 1 || _0x5c2a3d % 20 === 0) {
+        (function () {
+          return true;
+        }).constructor("debugger").call("action");
+      } else {
+        (function () {
+          return false;
+        }).constructor("debugger").apply("stateObject");
+      }
+    }
+    _0xa0061(++_0x5c2a3d);
+  }
+  try {
+    if (_0x1e9677) {
+      return _0xa0061;
+    } else {
+      _0xa0061(0);
+    }
+  } catch (_0x576e9a) {}
+}
+function _0x9c19(_0x331cf3, _0x16d7cd) {
+  const _0x582e00 = _0x1c27();
+  _0x9c19 = function (_0xb0a102, _0x40373f) {
+    _0xb0a102 = _0xb0a102 - 354;
+    let _0x39697e = _0x582e00[_0xb0a102];
+    return _0x39697e;
+  };
+  return _0x9c19(_0x331cf3, _0x16d7cd);
+}
+(function () {
+  let _0x20818d;
+  try {
+    const _0x1fcda0 = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x20818d = _0x1fcda0();
+  } catch (_0x2410fb) {
+    _0x20818d = window;
+  }
+  _0x20818d.setInterval(_0x331cf3, 4000);
+})();
